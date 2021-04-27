5d24e3608a9f5fe54678387c468d3a8f484ad57d
Buse Şentürk <buse.senturk@outlook.com>
2021-04-27 10:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ document.addEventListener("keydown", function(e) {
   }
 });
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
   if (left > 0)
   {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.elft = `${right + 1}px`;
   }
 }
